6bbf299866b9608ba69c97ac4614fbb3c4075edc
xyrlan <lobatopedro68@gmail.com>
2024-02-15 16:11:49 -0400

    fix suspense seachparams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import ParceirosSection from "@/components/ParceirosSection";
 import ReviewSection from "@/components/ReviewSection";
 import SobreSection from "@/components/SobreSection";
 import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 
 export default function Home() {
   const searchParams = useSearchParams()
   const section = searchParams.get('section');
-  
+
   useEffect(() => {
     if (section) {
       const element = document.getElementById(section);
@@ -22,12 +22,16 @@ export default function Home() {
 
 
   return (
-    <main>
-      <HeroSection />
-      <SobreSection />
-      <ReviewSection />
-      <ParceirosSection />
-      <FaleConoscoSection />
-    </main>
+    <Suspense fallback={<div className='h-screen w-full  flex items-center justify-center bg-[#292824]'>
+      <h1 className='fontnihon text-2xl lg:text-5xl text-white duration-300 motion-safe:animate-pulse'>NIHON</h1>
+    </div>}>
+      <main>
+        <HeroSection />
+        <SobreSection />
+        <ReviewSection />
+        <ParceirosSection />
+        <FaleConoscoSection />
+      </main>
+    </Suspense>
   );
 }
