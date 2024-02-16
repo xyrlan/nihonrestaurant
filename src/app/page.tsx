099f7acc3c766b9a2e7c7988eb37174ab2031cf3
xyrlan <lobatopedro68@gmail.com>
2024-02-16 14:38:03 -0400

    troquei de lugar fale conosco com nossas marcas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,8 +26,8 @@ export default function Home() {
       <HeroSection />
       <SobreSection />
       <ReviewSection />
-      <ParceirosSection />
       <FaleConoscoSection />
+      <ParceirosSection />
     </main>
   );
 }
