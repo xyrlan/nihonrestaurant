cb2f5cf5f7f1d841b4867532b03d2d016b404b98
xyrlan <lobatopedro68@gmail.com>
2024-02-22 14:54:52 -0400

    update SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Providers from "@/lib/providers";
 import { Suspense } from "react";
 import Image from "next/image";
+import Head from "next/head";
 
 
 const josepin = Josefin_Sans({ subsets: ["latin"] })
@@ -12,6 +13,11 @@ const josepin = Josefin_Sans({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Nihon Restaurante Japonês",
   description: " Restaurante e Sushi Bar localizado em Aquidauana MS, Há 12 anos, começamos com um sonho: fornecer comida e atendimento de qualidade em Aquiadauana MS.",
+  abstract: "Restaurante e Sushi Bar localizado em Aquidauana MS, Há 12 anos, começamos com um sonho: fornecer comida e atendimento de qualidade em Aquiadauana MS.",
+  keywords: "Restaurante, Aquidauana, Comida Japonesa, Sushi, Sashimi, Temaki, Yakisoba, Teppan, Aquidauana, MS, Mato Grosso do Sul, Brasil, Japão, Nihon, Nihon Restaurante, Nihon Aquidauana, Nihon Aquidauana MS, Nihon Aquidauana Mato Grosso do Sul, Nihon Aquidauana Brasil, Nihon Aquidauana Japão, Nihon Aquidauana Restaurante, Nihon Aquidauana Restaurante Japonês, Nihon Aquidauana Restaurante Japonês Aquidauana, Nihon Aquidauana Restaurante Japonês Aquidauana MS, Nihon Aquidauana Restaurante Japonês Aquidauana Mato Grosso do Sul, Nihon Aquidauana Restaurante Japonês Aquidauana Brasil, Nihon Aquidauana Restaurante",
+  category: "Restaurante",
+  
+  
 };
 
 
@@ -22,6 +28,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <Head>
+      <link
+          rel="canonical"
+          href="https://www.nihonrestaurante.com.br"
+          key="canonical"
+        />
+      </Head>
       <body className={`${josepin.className} bg-[#292824]`}>
         <Suspense fallback={<div className='h-screen w-full flex items-center justify-center bg-[#292824]'>
           <Image src={'/nihonspan.png'} width={400} height={400} alt='logonihon' className='h-12 md:h-20 w-auto duration-300 motion-safe:animate-pulse' />
