583b16652bd32ef7d546a17474d856b1663f4cb6
xyrlan <lobatopedro68@gmail.com>
2024-02-27 18:27:12 -0400

    Fix typo and improve readability in HeroSection component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -9,8 +9,9 @@ const HeroSection = () => {
       <Image src={'/nihonfachada.jpeg'} fill alt='fundo' className='w-full h-full absolute object-cover object-center'/>
       <motion.div initial={{ opacity: 0, y: 100 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true }} transition={{ duration: 1 }} className='border-4 border-white bg-black bg-opacity-50 p-10 max-w-lg z-20'>
         <h2 className='text-3xl md:text-5xl text-white font-bold mb-4 tracking-tighter '>AQUI COMEÇAM TRADIÇÕES.</h2>
-        <p className='text-white text-base sm:text-lg'>Há 12 anos em Aquidauana, temos orgulhos de fazer parte da vida
-          de tantas familias e suas tradições. Nós buscamos a
+        <p className='text-white text-base sm:text-lg'>
+          Há 12 anos em Aquidauana, temos orgulho de fazer parte da vida
+          de tantas famílias e suas tradições. Nós buscamos a
           qualidade e excelência em nossos pratos e produtos, com
           atendimento diferenciado que fez de nós, o segundo lar de muitas pessoas</p>
       </motion.div>
